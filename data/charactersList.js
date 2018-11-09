a3931cb3b0bfc2e5c86ba62283af98faf5c68892
JaviBQ92Legion <javier.gonzalezberenguel@gmail.com>
2018-11-09 13:18:57 +0100

    # character list elements type and comma

--- data/charactersList.js
+++ data/charactersList.js
@@ -1,42 +1,42 @@
 var enemies = [
-    bloodyeye,
-    death,
-    mummy,
-    owl,
-    phantom,
-    pirateskull,
-    skeleton,
-    spider,
-    undeadhand,
-    vampire
+    "bloodyeye",
+    "death",
+    "mummy",
+    "owl",
+    "phantom",
+    "pirateskull",
+    "skeleton",
+    "spider",
+    "undeadhand",
+    "vampire"
 ];
 
 var objectives = [
-    biscuit,
-	burger,
-	chococookie,
-	chocolate,
-	cupcake,
-	donut,
-	fries,
-	icecream,
-	pizza,
-	popcorn,
-	potatoes,
-	poti,
-	taco,
+    "biscuit",
+	"burger",
+	"chococookie",
+	"chocolate",
+	"cupcake",
+	"donut",
+	"fries",
+	"icecream",
+	"pizza",
+	"popcorn",
+	"potatoes",
+	"poti",
+	"taco"
 ];
 
 var transports = [
-    balloon,
-    bike,
-    bus,
-    //car, 
-    //moto,
-    plane,
-    //taxi,
-    train,
-    truck
+    "balloon",
+    "bike",
+    "bus",
+    //"car", 
+    //"moto",
+    "plane",
+    //"taxi",
+    "train",
+    "truck"
 ];
 
 function getEnemies() {
