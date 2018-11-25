44be68c6da0009a8f8f6a5b0a7b5d5a82901622a
JaviBQ92Legion <javier.gonzalezberenguel@gmail.com>
2018-11-25 22:54:25 +0100

    # enemy superposition

--- assets/loadEnemyBattle.js
+++ assets/loadEnemyBattle.js
@@ -1,7 +1,7 @@
 function loadEnemyBattle(enemyName) {
     var ec = document.getElementById("enemyCanvas");
     var eCtx = ec.getContext("2d");
-    // ctx.clearRect(0, 0, c.width, c.height);
+    eCtx.clearRect(0, 0, ec.width, ec.height);
     var playerImage = document.getElementById('marluxia');
     var enemyImage = document.getElementById(enemyName);
     eCtx.drawImage(playerImage, 0, 0);
