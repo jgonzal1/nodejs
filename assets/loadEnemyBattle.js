c938a0d3efa823d6368b961c67f6cb0a8481f371
JaviBQ92Legion <javier.gonzalezberenguel@gmail.com>
2018-11-25 22:50:14 +0100

    + battle elements and size correction

--- assets/loadEnemyBattle.js
+++ assets/loadEnemyBattle.js
@@ -2,10 +2,10 @@ function loadEnemyBattle(enemyName) {
     var ec = document.getElementById("enemyCanvas");
     var eCtx = ec.getContext("2d");
     // ctx.clearRect(0, 0, c.width, c.height);
-    // var playerImage = document.getElementById('marluxia');
+    var playerImage = document.getElementById('marluxia');
     var enemyImage = document.getElementById(enemyName);
-    // ctx.drawImage(playerImage,0,0);
-    eCtx.drawImage(enemyImage, 0, 0); // 260,0
+    eCtx.drawImage(playerImage, 0, 0);
+    eCtx.drawImage(enemyImage, 380, 0);
 }
 
 module.exports = loadEnemyBattle;
