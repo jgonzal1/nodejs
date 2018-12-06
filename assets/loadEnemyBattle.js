98c5e257447e694ed8b4ecd719677f648e7506b3
JaviBQ92Legion <javier.gonzalezberenguel@gmail.com>
2018-12-06 23:26:21 +0100

    + battleButtons

--- assets/loadEnemyBattle.js
+++ assets/loadEnemyBattle.js
@@ -1,5 +1,5 @@
 function loadEnemyBattle(enemyName) {
-    var ec = document.getElementById("battleCanvas");
+    var ec = document.getElementById("fightersCanvas");
     var eCtx = ec.getContext("2d");
     eCtx.clearRect(0, 0, ec.width, ec.height);
     var playerImage = document.getElementById('marluxia');
