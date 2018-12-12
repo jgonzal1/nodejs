53e51d2b71eb3112111f7a0b624484747cc55ac7
JaviBQ92Legion <javier.gonzalezberenguel@gmail.com>
2018-12-12 23:24:29 +0100

    # keymap.js centralization

--- data/keymap.js
+++ data/keymap.js
@@ -1,27 +1,205 @@
+var keypressMap = {
+"activateMutations":[123],
+"build":[42,66,98],
+"chatMenu":[67],
+"close":[99],
+"compare":[73],
+"controlVehicle":[94],
+"craft":[38],
+"debugModal":[126],
+"developerSwitch":[89,121],
+"disassembly":[40],
+"examine":[88,120],
+"exit":[27],
+"factions":[35],
+"fireMode":[70],
+"fleet":[102],
+"get":[103],
+"grabVehicle":[71],
+"help":[63,72,104],
+"ignoreRearestEnemy":[39],
+"inventory":[105],
+"journal":[74,106],
+"kick":[75,107],
+"killCount":[41],
+"leaveItem":[76,108],
+"listOfActions":[37],
+"manageInventory":[47],
+"moveEast":[54,68,100],
+"moveNE":[57],
+"moveNW":[55],
+"moveNorth":[56,87,119],
+"moveSE":[51],
+"moveSouth":[50,83,115],
+"moveSW":[49],
+"moveWest":[52,65,97],
+"music":[77,109],
+"no":[78,110],
+"open":[79],
+"options":[111],
+"pause":[44,46,53,80,112],
+"pickOrSearchNearest":[32],
+"playerInfo":[64],
+"quests":[81,113],
+"read":[114],
+"recraftLast":[45],
+"reload":[114],
+"research":[82],
+"run":[34],
+"safemode":[33],
+"sleep":[36],
+"swapInventoryLetters":[61],
+"takeOut":[116],
+"toggleStats":[122],
+"treatmentsModal":[84],
+"unArm":[95],
+"unWield":[85],
+"use":[117],
+"vehicleHandbrake":[86],
+"viewMorale":[118],
+"waitLong":[124],
+"wear":[43],
+"wield":[69,101]
+};
+
+var keymap = {
+    "activateMutations":["{"],
+    "addMission":["~M"],
+    "bionics":["Shift+B"],
+    "build":["*","B","b"],
+    "changeSkills":["~A"],
+    "changeWeather":["~h"],
+    "chatMenu":["C"],
+    "close":["c"],
+    "compare":["I"],
+    "controlVehicle":["^"],
+    "craft":["&"],
+    "craftIndefinedly":["Shift+C"],
+    "customizeKeymap":["?1"],
+    "debugModal":["~"],
+    "deleteAllItems":["~D"],
+    "developerSwitch":["y"],
+    "disassembly":["("],
+    "editCharacter":["~d"],
+    "editMap":["~g","~m"],
+    "editStats":["~T"],
+    "examine":["X","x"],
+    "exit":["ESC"],
+    "factions":["#"],
+    "fireMode":["F"],
+    "fireWielded":["Shift+F"],
+    "fleet":["f"],
+    "gameState":["~7"],
+    "get":["g"],
+    "goToSpawn":["Shift+G"],
+    "grabVehicle":["G"],
+    "help":["?","H","h"],
+    "ignoreRearestEnemy":["'"],
+    "inventory":["i"],
+    "journal":["J","j"],
+    "keys":["Shift+K"],
+    "kick":["k"],
+    "killAllNearby":["~K"],
+    "killCount":[")"],
+    "killNpcs":["~8"],
+    "leaveItem":["l"],
+    "listEnemies":["Shift+V"],
+    "listOfActions":["%"],
+    "login":["Shift+L"],
+    "manageInventory":["/"],
+    "moveDown":["S","s"],
+    "moveEast":["6","D","d"],
+    "moveMapViewDown":["DOWN"],
+    "moveMapViewLeft":["LEFT"],
+    "moveMapViewRight":["RIGH"],
+    "moveMapViewUp":["UP"],
+    "moveNE":["9"],
+    "moveNW":["7"],
+    "moveNorth":["8"],
+    "moveSE":["3"],
+    "moveSW":["1"],
+    "moveSouth":["2"],
+    "moveUp":["W","w"],
+    "moveWest":["4","A","a"],
+    "music":["M","m"],
+    "mutate":["~u"],
+    "no":["n"],
+    "open":["O"],
+    "options":["o"],
+    "pause":[".",",","5","P","p"],
+    "pickOrSearchNearest":[" "],
+    "playerInfo":["@"],
+    "quitGame":["Q"],
+    "read":["r"],
+    "reload":["R"],
+    "recraftLast":["-"],
+    "research":["r"],
+    "routePlanner":["~r"],
+    "run":["\""],
+    "saveQuit":["Ctrl+S"],
+    "scents":["~p"],
+    "searchItem":["~1"],
+    "seeWeather":["~o"],
+    "setHealth":["~H"],
+    "setNeeds":["~n"],
+    "setRandomClass":["~c"],
+    "setTime":["~t"],
+    "skills":["~S"],
+    "sleep":["$"],
+    "sorceries":["Shift+S"],
+    "sounds":["~s"],
+    "spawnEnemy":["~5","~6"],
+    "spawnPlace":["~E"],
+    "spawnVehicle":["~0"],
+    "statusWindow":["~@"],
+    "swapInventoryLetters":["="],
+    "takeOut":["t"],
+    "teleport":["~e"],
+    "teleportLong":["~3"],
+    "teleportShort":["~2"],
+    "testItems":["~k"],
+    "toggleSafemode":["!"],
+    "toggleStats":["z"],
+    "treatmentsModal":["T"],
+    "unArm":["_"],
+    "unWield":["U"],
+    "unlockAllMeleeAttacks":["~B"],
+    "unlockAllRecipes":["~C"],
+    "use":["u"],
+    "vehicleHandbrake":["V"],
+    "viewMessages":["Shift+M"],
+    "viewMorale":["v"],
+    "viewMutations":["~m"],
+    "waitLong":["|"],
+    "wear":["+"],
+    "wearWield":["~w","E","e"]
+};
+
+function getKeypressMap() {
+    return keypressMap;
+}
+function getKeymap() {
+    return keymap;
+}
+
+module.exports.getKeypressMap = getKeypressMap;
+module.exports.getKeymap = getKeymap;
+
+/*
 keyEventWhich	keyFunction	description
 _	unArm	Select unarmed style [Hint]
 -	recraftLast	Re-craft last recipe
 !	Toggle Safemode	
-?	help	
 ?1	customizeKeymap	To revert back to defaults the personalized-keymap.csv key bindings just delete it.
 ,	pause	Comma used to PickingUpItems; Twice Selects/Deselect all items [Hint]
-.	pause	
-'	ignoreRearestEnemy	
-"	run	
 (	disassembly	Disassemble items
-)	killCount	
-{	activateMutations	
 @	playerInfo	Open Character Status screen for Controlling Skills. TAB to navigate, enter to toggle activation
 *	build	Construct terrain
 /	manageInventory	Advanced inventory management [Hint]
-&	craft	
-#	factions	
 %	listOfActions	Access list of actions [Hint]
 ^	controlVehicle	Control vehicle [Hint]
 +	wear	Re-layer armour/clothing [Hint]
-<	FREE	ascendStairs
 =	swapInventoryLetters	Swap inventory letters [Hint]
->	FREE	descendStairs
 |	waitLong	Wait for several minutes [Hint]
 ~	debugModal	It will allow you to see things like the age/damage/freshness values of various items. Debug keys usage is essentially CHEATING; unless you are in a situation caused by bugs (e.g. spawning in a windowless doorless room) or testing game features.
 ~@	statusWindow	Shows the status window. Same window as the normal @ would show.
@@ -50,84 +228,44 @@ _	unArm	Select unarmed style [Hint]
 ~m	editMap	Allows you to edit the overmap. Locations in red will not be changed. Follow on screen instructions.
 ~m	viewMutations	Shows the levels of the various threshold mutations.
 ~n	Set needs	Allows you to see and edit the Hunger; Thirst; Fatigue and Vitamins levels of the NPC/player.
-~o	seeWeather	
-~p	scents	
 ~r	routePlanner	Moves player to selected location; by walking normally.
 ~S	skills	Allows editing of the NPC; or player skills
-~s	sounds	
 ~t	setTime	Change the current year; season; day; hour; minute; and amount of turns.
 ~T	Edit Stats	Edit the player or NPC maximum stats.
 ~u	mutate	Edits mutations/traits of the NPC/player using the same interface as the previous menu.
 ~w	Wear/Wield	Selects item to (un)wear/(un)wield an item from NPC/Player's inventory
 $	sleep	Sleep [Hint]
-1	moveSW	
-2	moveSouth	
-3	moveSE	
-4	moveWest	
-5	pause	
-6	moveEast	
-7	moveNW	
-8	moveNorth	
-9	moveNE	
-A	moveWest	
-a	moveWest	
 B	build	ex-Butcher a corpse [Hint]
 b	build	ex-Butcher a corpse [Hint]
 C	chatMenu	Menu for Talking to NPC's or Yelling. Chat with an NPC [Hint]
 c	close	Close door or window [Hint]
 Ctrl+S	saveQuit	Save and quit [Hint]
-D	moveEast	
-d	moveEast	
-DOWN	moveMapViewDown	
 e	wield	Wield item [Hint]. If you previously selected a martial arts style through _ it'll revert to that MA style instead. The currently wielded item will be automatically placed in your inventory or you'll be asked if you want to drop it if there's no space left.
 E	wield	Wield item [Hint]. If you previously selected a martial arts style through _ it'll revert to that MA style instead. The currently wielded item will be automatically placed in your inventory or you'll be asked if you want to drop it if there's no space left.
 ESC	exit	Exit dialog or screen / Cancel
 F	fireMode	Toggle attack mode of wielded item [Hint]. Fire wielded item [Hint]
-f	fleet	
 G	grabVehicle	Grab a nearby vehicle / Release vehicle [Hint]
 g	get	Pick item(s) up [Hint]
-H	help	
-h	help	
 I	compare	Compare two items [Hint]
-i	inventory	
 j	journal	missions
 J	journal	missions
 k	kick	Smash nearby object
 l	leaveItem	Drop item [Hint]. Items to adjacent tile [Hint] also displays the inventory screen.Type the number of items you want to drop. Press again the key of the inventory item you want to divide (in some cases; this will cause the item letter to be displayed as #). Press RETURN. Mounted turrets from vehicle mods are reloaded using either of the [D]rop commands.
-LEFT	moveMapViewLeft	
-M	music	
-m	music	
-n	no	
-o	options	
 O	open	Open door or window [Hint]
-p	pause	
-P	pause	
 Q	quitGame	Commit suicide [Hint]
 R	read,reload	Read a book or a magazine [Hint],Reload wielded item [Hint]
-r	research	
-RIGH	moveMapViewRight	
-S	moveDown	
-s	moveDown	
 Shift+B	bionics	View/Activate bionics
 Shift+C	craftIndefinedly	Craft for as long as possible
 Shift+F	fireWielded	Burst-fire wielded item [Hint]
-Shift+G	goToSpawn	
-Shift+K	keys	
-Shift+L	login/logout	
 Shift+M	viewMessages	View message log
-Shift+S	sorceries	
 Shift+V	listEnemies	List all items/creatures around the player [Hint]
 SPACE	pickOrSearchNearest	Pick or search object
 T	treatmentsModal	diplomacy
 t	takeOut	Throw item [Hint] / Take off worn item
 U	unWield	Unload or empty wielded or nearby item [Hint]
 u	use	eat, drink or consume item [Hint]. Apply or use currently wielded item. Apply or use an item [Hint]
-UP	moveMapViewUp	
 V	vehicleHandbrake	Vehicle handbrake [Hint]
-v	viewMorale	
-w	moveUp	
-W	moveUp	
 X	examine	Peek around corners
 x	examine	Examine or interact with nearby world object [Hint].
 y	developerSwitch	also yes
-z	toggleStats	
+*/
