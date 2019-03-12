ebbdc7765db1d6de4dd8299a0e1a9d1b82293aa3
Javier Gonzalez Berenguel <67456814@iecisa.corp>
2019-03-12 16:14:33 +0100

    +t setTimeout() in Promises

--- promiseApp.js
+++ promiseApp.js
@@ -1,10 +1,12 @@
+console.log("This is the first line code in app.js.");
+// Keep the line above as the first line of code
+// Write your code here:
+
 const inventory = {
   sunglasses: 1900,
   pants: 1088,
   bags: 1344
 };
-
-// Write your code below:
 const sunglassesOrderExecutor = (resolve, reject) => {
   if (inventory["sunglasses"] > 0) {
     resolve('Sunglasses order processed.');
@@ -16,5 +18,12 @@ function orderSunglasses() {
   return new Promise(sunglassesOrderExecutor);
 }
 const orderPromise = orderSunglasses();
-
 console.log(orderPromise);
+
+const usingSTO = () => {
+  console.log("Delayed print");
+};
+setTimeout(usingSTO, 2999);
+
+// Keep the line below as the last line of code:
+console.log("This is the last line of code in app.js.");
