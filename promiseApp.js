f2b0ebf6b11c466b342b7959a6765388517c91cd
Javier Gonzalez Berenguel <67456814@iecisa.corp>
2019-03-12 18:35:40 +0100

    +t chaining promises with storage library example

--- promiseApp.js
+++ promiseApp.js
@@ -1,21 +1,10 @@
 console.log("This is the first line code in app.js.");
+const {checkInventory, processPayment, shipOrder} = require('./promiseStoreLibrary.js');
 
 const inventory = {
   sunglasses: 1900,
   pants: 1088,
   bags: 1344
-};
-const checkInventory = (order) => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const inStock = order.every(item => inventory[item[0]] >= item[1]);
-      if (inStock) {
-        resolve(`Thank you. Your order was successful.`);
-      } else {
-        reject(`We're sorry. Your order could not be completed because some items are sold out.`);
-      }
-    }, 500);
-  });
 };
 const sunglassesOrderExecutor = (resolve, reject) => {
   if (inventory["sunglasses"] > 0) {
@@ -35,8 +24,9 @@ const handleSuccess = (resolvedValue) => {
 const handleFailure = (rejectionReason) => {
   console.log(rejectionReason);
 };
-const order = [['sunglasses', 1], ['bags', 2]];
-checkInventory(order).then(handleSuccess,handleFailure);
+const order1 = [['sunglasses', 1], ['bags', 2]];
+checkInventory(order1).then(handleSuccess,handleFailure);
+
 
 const promiseExample2 = new Promise(
   (resolve, reject) => {
@@ -56,6 +46,24 @@ promiseExample2
     console.log(rejectionReason);
   });
 // Prints: 'onResolvePrint after ".then"' if resolve
+
+const order3 = {
+  items: [['sunglasses', 1], ['bags', 2]],
+  giftcardBalance: 79.82
+};
+checkInventory(order3)
+.then((resolvedValueArray) => {
+  return processPayment(resolvedValueArray);
+})
+.then((resolvedValueArray) => {
+  return shipOrder(resolvedValueArray);
+})
+.then((successMessage) => {
+  console.log(successMessage);
+})
+.catch((errorMessage) => {
+  console.log(errorMessage);
+});
 
 const usingSTO = () => {
   console.log("Delayed print");
