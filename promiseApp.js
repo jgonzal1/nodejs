95b9d63e5ddb63dee5c501b542e57d42d7baad74
Javier Gonzalez Berenguel <67456814@iecisa.corp>
2019-03-21 15:58:47 +0100

    + onFulfulill, onReject promises

--- promiseApp.js
+++ promiseApp.js
@@ -71,13 +71,16 @@ const checkPants = checkAvailability('pants', 'Favorite Supply Co.');
 
 const checkBags = checkAvailability('bags', 'Favorite Supply Co.');
 
+const onFulfill = (itemsArray) => {
+  console.log(`Items checked: ${itemsArray}`);
+  console.log(`Every item was available from the distributor. Placing order now.`);
+};
+const onReject = (rejectionReason) => {
+	console.log(rejectionReason);
+};
 const myPromises = Promise.all([checkSunglasses, checkPants, checkBags])
- .then((arrayOfValues) => {
-  handleSuccess(arrayOfValues);
- })
- .catch((rejectionReason) => {
-  handleFailure(rejectionReason);
- });
+.then(onFulfill)
+.catch(onReject);
 
 const usingSTO = () => {
   console.log("Delayed print");
